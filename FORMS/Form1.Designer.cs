6a1ee63d0ecd3fd6f898a225504b52cfadb04d17
GrIgar45 <GrIgar45@gmail.com>
2016-04-20 15:21:36 +0300

    Creating a project

--- FORMS/Form1.Designer.cs
+++ FORMS/Form1.Designer.cs
@@ -23,9 +23,17 @@
     /// содержимое этого метода с помощью редактора кода.
     /// </summary>
     private void InitializeComponent() {
-      this.components = new System.ComponentModel.Container();
+      this.SuspendLayout();
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(830, 344);
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
